4fecc8cceac88ed301de76bbd24be0a0d9e05e96
athebolt <themanwiththeplan01@icloud.com>
2023-10-06 16:31:20 -0400

    Game Updated
    -Added Player
    -Added OB
    -Added Player walk and jump
    -Added Player Script
    -Updated GameManager script

--- Homework01_Program1/Assets/Scripts/GameManager.cs
+++ Homework01_Program1/Assets/Scripts/GameManager.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public static class GameManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static string GAME_SCENE = "SampleScene";
 }
---
+++ Homework01_Program1/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static GameManager;
+
+public class Player : MonoBehaviour
+{
+    //private variables
+    private Rigidbody2D playerRigidBody;
+    private float inputHorizontal;
+    private bool isGrounded;
+
+    //public variables will appear in the inspector
+    public float movementSpeed;
+    public float jumpForce;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getting the rigidbody component from the player and assigning it to the variable "playerRigidBody"
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayerLateral();
+        playerJump();
+    }
+
+    //temp movement will be improved
+    private void movePlayerLateral()
+    {
+        //will get horizontal input of the keyboard (a or d/left or right arrow keys)
+        //left is -1
+        //right is 1
+        //none is 0
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        playerRigidBody.velocity = new Vector2(movementSpeed * inputHorizontal, playerRigidBody.velocity.y);
+    }
+
+    private void playerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("OB"))
+        {
+            //reload the screen if there is a collsion in the boundary
+            SceneManager.LoadScene(GAME_SCENE);
+        }
+        else if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
